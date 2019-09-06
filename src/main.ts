9ad316363387d38c62a273cb8afbe1873b999f7c
kiwenlau <kiwenlau@126.com>
2019-09-06 19:51:48 +0800

    使用Fundebug监控异常

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as fundebug from 'fundebug-javascript';
+
+fundebug.init({
+  apikey: 'b351f9db1549fb2ecbbb79feece8ddb473f8f1dddf1090a68dc85db33075667a'
+});
+
+fundebug.test();
+
 if (environment.production) {
   enableProdMode();
 }
